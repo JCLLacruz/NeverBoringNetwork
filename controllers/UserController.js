26271529e8ea642ade5f5589415550e792f73f3b
Juan Carlos <juancarloslacruzlacruz@outlook.com>
2024-05-10 18:22:56 +0200

    middleware authentication created. login method UserController created

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -8,9 +8,27 @@ const UserController = {
             const user = await User.create(req.body)
             res.status(201).send({msg:'User created', user});
         } catch (error) {
-            
+            console.error(error);
         }
-    }
+    },
+    async login(req,res) {
+        try {
+            const user = await User.findOne({
+                email: req.body.email,
+            })
+            const token = {
+                token: jwt.sign({_id: user._id},jwt_secret),
+                device
+        };
+        if(user.tokens.length > 4) user.tokens.shift();
+        user.tokens.push(token);
+        user.online = true;
+        await user.save();
+        res.send({msg: `Welcome ${user.name.first}`, user, token});
+        } catch (error) {
+            console.error(error);
+        }
+    },
 };
 
 module.exports = UserController;
