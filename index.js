c1f1b5a7843ff1356e71047f20f85e9a77ff78a1
Juan Carlos <juancarloslacruzlacruz@outlook.com>
2024-05-10 13:55:11 +0200

    fixes mounting server

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const {dbConnection} = require('./config/config.js');
 const app = express();
+dbConnection();
 
 const PORT = 3001;
 
 app.use(express.json());
+//app.use()
 
-app.list(PORT, ()=> console.log(`Server started on port ${PORT}`));
+app.listen(PORT, ()=> console.log(`Server started on port ${PORT}`));
 
