51bf825a2b1569f500cd2fa33d13edc2835fc93b
Juan Carlos <juancarloslacruzlacruz@outlook.com>
2024-05-12 08:59:54 +0200

    logout UserController created

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -20,18 +20,18 @@ const UserController = {
 				html: `<h3> Welcome to NeverBoringNetwork, only one step more to enjoy!</h3>
 				<a href=${url}>Click to confirm your email</a>`,
 			});
-			res.status(201).send({ msg: `The user's email must be confirmed`, user });
+			res.status(201).send({ msg: `The user's email must be confirmed.`, user });
 		} catch (error) {
 			console.error(error);
 			switch (true) {
 				case error.keyPattern['email']:
-					res.status(400).send({ msg: 'The email has already been used', error });
+					res.status(400).send({ msg: 'The email has already been used.', error });
 					break;
 				case error.keyPattern['username']:
-					res.status(400).send({ msg: 'The username has already been used', error });
+					res.status(400).send({ msg: 'The username has already been used.', error });
 					break;
 				default:
-					res.status(500).send({ msg: 'Server error', error });
+					res.status(500).send({ msg: 'Server error.', error });
 					break;
 			}
 		}
@@ -41,7 +41,7 @@ const UserController = {
             const emailToken = req.params.emailToken;
             const payload = jwt.verify(emailToken, jwt_secret);
             await User.updateOne({email: payload.email},{$set: {emailConfirmed: true}});
-            res.status(201).send({ msg: 'User email was confirmed' });
+            res.status(201).send({ msg: 'User email was confirmed.' });
         } catch (error) {
             console.error(error);
             res.status(500).send(error);
@@ -53,7 +53,7 @@ const UserController = {
 				email: req.body.email,
 			});
 			if (!user) {
-				res.send({ msg: 'Wrong email or password' });
+				res.send({ msg: 'Wrong email or password.' });
 			}
 			const token = {
 				token: jwt.sign({ _id: user._id }, jwt_secret),
@@ -63,7 +63,7 @@ const UserController = {
 			user.tokens.push(token);
 			user.online = true;
 			await user.save();
-			res.send({ msg: `Welcome ${user.name.first}`, user, token });
+			res.send({ msg: `Welcome ${user.name.first}.`, user, token });
 		} catch (error) {
 			console.error(error);
 			res.send(error);
@@ -75,16 +75,27 @@ const UserController = {
 			res.send({ msg: 'Online users', users });
 		} catch (error) {
 			console.error(error);
-			res.status(500).send({ msg: 'Server error', error });
+			res.status(500).send({ msg: 'Server error.', error });
 		}
 	},
-    async userById(req,res) {
+    async userFindById(req,res) {
         try {
             const user = await User.findOne({_id: req.params._id});
-            res.send({msg: `User with ID: ${req.params._id} was found.`, user});
+            res.send({msg: `User with id: ${req.params._id} was found.`, user});
         } catch (error) {
             console.error(error);
-            res.status(500).send(error);
+            res.status(500).send({msg: `The user with id: ${error.value} does not exist in the database.`, error});
+        }
+    },
+    async logout(req,res) {
+        try {
+            const user = await User.findByIdAndUpdate({_id: req.params._id},{$pull: {tokens: {token: req.headers.authorization}}}, {new: true});
+            user.online = false;
+            await user.save();
+            res.send(({msg: 'User logged out', user}))
+        } catch (error) {
+            console.error(error);
+            res.status(500).send({msg: `User not logged out properly.`, error});
         }
     }
 };
--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const UserController = require('../controllers/UserController');
 const router = express.Router();
+const { authentication } = require('../middleware/authentication.js');
 
-
-router.get('/onlineusers',UserController.onlineUsers);
+router.get('/onlineusers', UserController.onlineUsers);
 router.get('/confirm/:emailToken', UserController.confirmUser);
-router.get('/id/:_id', UserController.userById);
-router.post('/',UserController.register);
-router.post('/login',UserController.login);
+router.get('/id/:_id', UserController.userFindById);
+router.get('/logout/:_id', authentication, UserController.logout);
+router.post('/', UserController.register);
+router.post('/login', UserController.login);
 
 module.exports = router;
