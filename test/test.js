39459af6dbe4a157f4bc71ca5947b33d62418a8c
Juan Carlos <juancarloslacruzlacruz@outlook.com>
2024-05-13 15:24:02 +0200

    test for login user correct

--- test/test.js
+++ test/test.js
@@ -1,34 +1,44 @@
 const request = require('supertest');
 const app = require('../index.js');
-const User = require('../')
+const User = require('../models/User.js');
 
 describe('Endpoints testing', () => {
-    afterAll(async ()=>{
-        return await User.deleteMany({});
-    })
-    const user = {
-    username: 'testUser',
-    email: 'testusers@getMaxListeners.com',
-    password: 'holahola',
-    birthday: '1988-06-15',
-    name: {first: 'FirstNameTest',last: 'LastNameTest'},
-    }
-    test ('register', async () => {
+	afterAll(async () => {
+		return await User.deleteMany({});
+	});
+	const user = {
+		username: 'testUser',
+		email: '19lacruz88@gmail.com',
+		password: 'holahola',
+		birthday: '1988-06-15',
+		name: { first: 'FirstNameTest', last: 'LastNameTest' },
+	};
+	test('register', async () => {
+		const res = await request(app).post('/users').send(user).expect(201);
+		const { _id, emailConfirmed, birthday, role, tokens, FollowIds, FollowerIds, PostIds, password, createdAt, updatedAt, __v } = res.body.user;
+		const testUser = {
+			...user,
+			_id,
+			emailConfirmed,
+			birthday,
+			role,
+			tokens,
+			FollowIds,
+			FollowerIds,
+			PostIds,
+			password,
+			createdAt,
+			updatedAt,
+			__v,
+		};
+		const dataBaseUser = res.body.user;
+		expect(dataBaseUser).toEqual(testUser);
+	});
+	test ('login', async () => {
         const res = await request(app)
-        .post('/users')
-        .send(user)
-        .expect(201)
-        const testUser = {
-            ...user,
-            _id: res.body.user._id,
-            emailConfirmed: false,
-            role: res.body.user.role,
-            tokens: res.body.user.tokens, 
-            password: res.body.user.password,
-            createdAt: res.body.user.createdAt,
-            updatedAt: res.body.user.updatedAt
-        }
-        const dataBaseUser = res.body.user;
-        expect(dataBaseUser).toEqual(testUser);
+        .post('/users/login')
+        .send({email: user.email,password: user.password})
+        expect(200)
+        expect(res.body.msg).toBe(`Welcome ${user.name.first}.`)
     })
 });
