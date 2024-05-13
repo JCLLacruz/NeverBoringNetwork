996f3d2e71e39c127c42c01f95ad12fdc50e3f28
Juan Carlos <juancarloslacruzlacruz@outlook.com>
2024-05-13 10:05:02 +0200

    login isMatch condiction added

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -10,7 +10,7 @@ const UserController = {
 			if (req.body.username == '' || req.body.email == '' || req.body.password == '' || req.body.birthday == '') {
 				res.send({ msg: 'Please fill out all required fields.' });
 			}
-			const password = bcrypt.hashSync(req.body.password, 10);
+			const password = await bcrypt.hash(req.body.password, 10);
 			const user = await User.create({ ...req.body, password, role: 'user', emailConfirmed: false });
 			const emailToken = jwt.sign({ email: req.body.email }, jwt_secret, { expiresIn: '48h' });
 			const url = 'http://localhost:3001/users/confirm/' + emailToken;
@@ -36,24 +36,28 @@ const UserController = {
 			}
 		}
 	},
-    async confirmUser(req, res) {
-        try {
-            const emailToken = req.params.emailToken;
-            const payload = jwt.verify(emailToken, jwt_secret);
-            await User.updateOne({email: payload.email},{$set: {emailConfirmed: true}});
-            res.status(201).send({ msg: 'User email was confirmed.' });
-        } catch (error) {
-            console.error(error);
-            res.status(500).send(error);
-        }
-    },
+	async confirmUser(req, res) {
+		try {
+			const emailToken = req.params.emailToken;
+			const payload = jwt.verify(emailToken, jwt_secret);
+			await User.updateOne({ email: payload.email }, { $set: { emailConfirmed: true } });
+			res.status(201).send({ msg: 'User email was confirmed.' });
+		} catch (error) {
+			console.error(error);
+			res.status(500).send(error);
+		}
+	},
 	async login(req, res) {
 		try {
 			const user = await User.findOne({
 				email: req.body.email,
 			});
 			if (!user) {
-				res.send({ msg: 'Wrong email or password.' });
+				return res.status(400).send({ msg: 'Wrong email or password.' });
+			}
+			const isMatch = await bcrypt.compare(req.body.password,user.password)
+			if (!isMatch) {
+				return res.status(400).send({ msg: 'Wrong email or password.' });
 			}
 			const token = {
 				token: jwt.sign({ _id: user._id }, jwt_secret),
@@ -61,7 +65,6 @@ const UserController = {
 			};
 			if (user.tokens.length > 4) user.tokens.shift();
 			user.tokens.push(token);
-			user.online = true;
 			await user.save();
 			res.send({ msg: `Welcome ${user.name.first}.`, user, token });
 		} catch (error) {
@@ -69,7 +72,7 @@ const UserController = {
 			res.send(error);
 		}
 	},
-	async onlineUsers(req, res) {
+	async allOnlineUsers(req, res) {
 		try {
 			const users = await User.find({ online: true });
 			res.send({ msg: 'Online users', users });
@@ -78,26 +81,41 @@ const UserController = {
 			res.status(500).send({ msg: 'Server error.', error });
 		}
 	},
-    async userFindById(req,res) {
-        try {
-            const user = await User.findOne({_id: req.params._id});
-            res.send({msg: `User with id: ${req.params._id} was found.`, user});
-        } catch (error) {
-            console.error(error);
-            res.status(500).send({msg: `The user with id: ${error.value} does not exist in the database.`, error});
-        }
-    },
-    async logout(req,res) {
-        try {
-            const user = await User.findByIdAndUpdate({_id: req.params._id},{$pull: {tokens: {token: req.headers.authorization}}}, {new: true});
-            user.online = false;
-            await user.save();
-            res.send(({msg: 'User logged out', user}))
-        } catch (error) {
-            console.error(error);
-            res.status(500).send({msg: `User not logged out properly.`, error});
-        }
-    }
+	async userFindById(req, res) {
+		try {
+			const user = await User.findOne({ _id: req.params._id });
+			res.send({ msg: `User with id: ${req.params._id} was found.`, user });
+		} catch (error) {
+			console.error(error);
+			res.status(500).send({ msg: `The user with id: ${error.value} does not exist in the database.`, error });
+		}
+	},
+	async logout(req, res) {
+		try {
+			const user = await User.findByIdAndUpdate({ _id: req.params._id }, { $pull: { tokens: { token: req.headers.authorization } } }, { new: true });
+			user.online = false;
+			await user.save();
+			res.send({ msg: 'User logged out', user });
+		} catch (error) {
+			console.error(error);
+			res.status(500).send({ msg: `User not logged out properly.`, error });
+		}
+	},
+	async getOnline(req, res) {
+		try {
+			const user = await User.findOne({ _id: req.params._id });
+			if (!user.online) {
+				user.online = true;
+				await user.save();
+				return res.send({ msg: `User with Id: ${req.params._id} ist online`, user });
+			} else {
+				return res.send({ msg: `User with Id: ${req.params._id} ist offline`, user });
+			}
+		} catch (error) {
+			console.error(error);
+			res.status(500).send({ msg: `User status not changed.`, error });
+		}
+	},
 };
 
 module.exports = UserController;
