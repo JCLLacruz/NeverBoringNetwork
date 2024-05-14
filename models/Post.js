b65cc0105f26067df8b5b7c698d11c04a4950ba6
Juan Carlos <juancarloslacruzlacruz@outlook.com>
2024-05-14 13:03:04 +0200

    fixed PostModel

--- models/Post.js
+++ models/Post.js
@@ -11,14 +11,10 @@ const PostSchema = new mongoose.Schema({
         required: [true, "Please enter a body"],
     },
     image_path: String,
-<<<<<<< HEAD
-    UserId: ObjectId,
-=======
     UserId: {
         type: ObjectId,
         ref: "User"
     },
->>>>>>> 41a050264399cc024f3f8d235e3e3acbc43319ad
     //HobbyId: ObjectId,
     status: String,
     likes: [{
