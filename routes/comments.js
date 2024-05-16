a56eee9e536c340fb98c0f067ddd17f689764ae0
juan salas <juanjo.salas.dev@gmail.com>
2024-05-16 13:21:04 +0200

    isAuthor

--- routes/comments.js
+++ routes/comments.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const router = express.Router()
 const CommentController = require("../controllers/CommentController");
-const { authentication } = require("../middleware/authentication");
+const { authentication, isAuthor } = require("../middleware/authentication");
 const {uploadCommentImages} = require('../middleware/multer.js')
 
 router.post("/", authentication, uploadCommentImages.single('commentImg'), CommentController.create);
-router.put("/id/:_id", authentication, CommentController.update);
-router.delete("/id/:_id", authentication, CommentController.delete);
+router.put("/id/:_id", authentication, isAuthor, CommentController.update);
+router.delete("/id/:_id", authentication, isAuthor, CommentController.delete);
 router.get("/", authentication, CommentController.getAll);
 router.put("/like/:_id", authentication, CommentController.like);
 router.delete("/like/:_id", authentication, CommentController.dislike);
