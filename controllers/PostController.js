065d391801e8cf93b6b0629b92a3cc6c4eb4b72b
juan salas <juanjo.salas.dev@gmail.com>
2024-05-17 13:19:38 +0200

    swagger

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -98,8 +98,7 @@ const PostController = {
             res.send({ msg: "Like delete ", post });
         } catch (error) {
             console.error(error)
-            res.status(500).send({ msg: "There was a problem trying to remove the post"})
-    
+            res.status(500).send({ msg: "There was a problem trying to remove the like"})
         }
     }  
 };
---
+++ docs/posts.js
@@ -0,0 +1,27 @@
+module.exports = {
+    paths: {
+      "/posts": {
+        get: {
+          tags: {
+            Posts: "Get Posts",
+          },
+          description: "Get posts",
+          operationId: "getPosts",
+          parameters: [],
+          responses: {
+            200: {
+              description: "Posts were obtained",
+              content: {
+                "application/json": {
+                  schema: {
+                    $ref: "#/components/schemas/Post",
+                  },
+                },
+              },
+            },
+          },
+        },
+      },
+    },
+  };
+  
